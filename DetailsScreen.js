16cd7018ff63eba57bac7a69eaea56f9d9f48df0
leandrofp <leandro.fernandezp@gmail.com>
2018-06-25 13:15:29 -0300

    Agregados Encabezado y Pie de Lista con margen para no ser tapado por el Tab

--- DetailsScreen.js
+++ DetailsScreen.js
@@ -9,76 +9,37 @@ class DetailsScreen extends React.Component {
 
     constructor(){
         super()
-        // state = {
-        //     users: []
-        //   }
+ 
     }
-
-    // componentDidMount(){
-    //     this.setState({users : this.props.tabnavi.people})
-    // }
 
     render() {
 
-
-        // const list = [
-        //     {
-        //       name: 'Amy Farha',
-        //       subtitle: 'Vice President'
-        //     },
-        //     {
-        //       name: 'Chris Jackson',
-        //       subtitle: 'Vice Chairman'
-        //     },
-        //   ]
-        //   return(
-        //   <View>
-        //     {
-        //       list.map((l, i) => (
-        //         <ListItem
-        //           key={i}
-        //           title={l.name}
-        //           subtitle={l.subtitle}
-        //         />
-        //       ))
-        //     }
-        //   </View>
-        // );
-      
-        const list = [
-            {
-              name: 'Amy Farha',
-              subtitle: 'Vice President'
-            },
-            {
-              name: 'Chris Jackson',
-              subtitle: 'Vice Chairman'
-            },
-        ]
-  
         //console.log(this.props.tabnavi.people)
         
         return (
-            <View style={{ flex: 1, justifyContent: 'center', /*alignItems: 'center' ,*/ backgroundColor:'yellow'}}>         
+            <View style={{ flex: 1, justifyContent: 'center', backgroundColor:'yellow'}}>     
+                {/*alignItems: 'center'  NO ANDA!!!!!*/}    
                 <View style={{marginTop: 45}}>
                 <Text style={{fontSize:14}}>Contador: {this.props.tabnavi.count1}</Text>            
                 </View>
                 <View> 
                  
                     <FlatList
+                        ListHeaderComponent={<Text>Inicio Pagina</Text>}
                         data={this.props.tabnavi.people}
                         renderItem={({item}) =>         
                             (
                                 this.props.tabnavi.people.map((item, i) => 
-                                    <ListItem
+                                <ListItem
                                     key={i}
                                     title={item.name.first}
                                     subtitle={item.email}
-                                    />
+                                />
                                 )
                             )
-                  
-                            }  /> 
+                        }
+                        ListFooterComponent={<Text style={{marginBottom:45}}>Fin de Pagina</Text>}
+                    /> 
               
                 </View>  
             </View>
